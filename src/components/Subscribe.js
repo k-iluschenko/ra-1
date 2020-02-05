7f3e8d3d6fbce79d92c21c59f4895d22d1524290
Kirill.Iluschenko <kirill.i@nekidaem.ru>
2020-02-05 15:14:33 +0300

    refactoring Subscribe.js

--- src/components/Subscribe.js
+++ src/components/Subscribe.js
@@ -4,56 +4,50 @@ class Subscribe extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      sendForm: false //не заполнена форма
+      sendForm: false
     };
   }
 
   onSubmit = (event) => {
     event.preventDefault();
-    let subscribe = '';
     const radio = document.subscribeForm.subscribe;
-    for (let i = 0; i < radio.length; i++) {
-      if (radio[i].checked) {
-        subscribe = radio[i].value;
-        this.setState({ sendForm: true });
-      }
-    }
-    
-    this.props.history.push(`?subscribe=${subscribe}`);
+    const checkedRadio = Array.from(radio).find(item => item.checked);
+    const subscribe = checkedRadio ? checkedRadio.value : '';
+    this.setState({ sendForm: true })
+
+    this.props.history.push(`?subscribe=${ subscribe }`);
   }
 
   render() {
-    if (!this.state.sendForm) {
-      return (
-        <div className="subscribe__wrapper">
-          <h2 className="subscribe__title">подписаться на рассылку выгодных предложений</h2>
-          <form onSubmit={this.onSubmit} className="subscribe__radios" action="" name='subscribeForm'>
-            <label className="subscribe__radio_label">
-              <input className="subscribe__radio" type="radio" name="subscribe" value="women" />
-              <div className="subscribe__radio_text">Женское</div>
-            </label>
-            <label className="subscribe__radio_label">
-              <input className="subscribe__radio" type="radio" name="subscribe" value="men" />
-              <div className="subscribe__radio_text">Мужское</div>
-            </label>
-            <label className="subscribe__radio_label">
-              <input className="subscribe__radio" type="radio" name="subscribe" value="both" required/>
-              <div className="subscribe__radio_text">Всё</div>
-            </label>
-            <input className="subscribe__email" type="email" placeholder="Ваш e-mail" required />
-            <input className="subscribe__submit" type="submit" value="ПОДПИСАТЬСЯ" />
-          </form>
-        </div>
-      )
-    }
-    else {
+    if (this.state.sendForm) {
       return (
         <div className="subscribe__wrapper">
           <h2 className="subscribe__title">подписаться на рассылку выгодных предложений</h2>
           Подписка оформлена! Спасибо!
-         </div>
-      )
+        </div>
+      );
     }
+    return (
+      <div className="subscribe__wrapper">
+        <h2 className="subscribe__title">подписаться на рассылку выгодных предложений</h2>
+        <form onSubmit={ this.onSubmit } className="subscribe__radios" action="" name="subscribeForm">
+          <label className="subscribe__radio_label">
+            <input className="subscribe__radio" type="radio" name="subscribe" value="women"/>
+            <div className="subscribe__radio_text">Женское</div>
+          </label>
+          <label className="subscribe__radio_label">
+            <input className="subscribe__radio" type="radio" name="subscribe" value="men"/>
+            <div className="subscribe__radio_text">Мужское</div>
+          </label>
+          <label className="subscribe__radio_label">
+            <input className="subscribe__radio" type="radio" name="subscribe" value="both" required/>
+            <div className="subscribe__radio_text">Всё</div>
+          </label>
+          <input className="subscribe__email" type="email" placeholder="Ваш e-mail" required/>
+          <input className="subscribe__submit" type="submit" value="ПОДПИСАТЬСЯ"/>
+        </form>
+      </div>
+    );
   }
 }
 
